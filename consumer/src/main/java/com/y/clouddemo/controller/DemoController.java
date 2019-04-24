1d9ad95d8fcdd0e7ac8ac4e75ba97429da387e25
lei <lei.yang@huilianyi.com>
2019-04-24 15:11:14 +0800

    11111

--- consumer/src/main/java/com/y/clouddemo/controller/DemoController.java
+++ consumer/src/main/java/com/y/clouddemo/controller/DemoController.java
@@ -5,10 +5,12 @@ import com.y.clouddemo.service.RibbonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@RequestMapping("/user")
 @RestController
 public class DemoController {
 
